3756f333b1009ec81c508738157fbc85fab61bd1
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-14 00:59:58 +0530

    sql tables created

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -2,16 +2,25 @@ package com.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
-	@Size(min = 2,message = "Name should have at least 2 characters")
-	private String name;
 	
 	@Past
 	private Date birthDate;
+	
+	@Size(min = 2,message = "Name should have at least 2 characters")
+	private String name;
 	
 	public User(Integer id, String name, Date birthDate) {
 		super();
