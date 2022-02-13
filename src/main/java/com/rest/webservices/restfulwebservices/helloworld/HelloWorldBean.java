54f1b5e2235454ad7b051a071d18cc8f9dff63a6
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-14 00:05:35 +0530

    first commit

---
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String string) {
+		this.message=string;
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+	
+}
