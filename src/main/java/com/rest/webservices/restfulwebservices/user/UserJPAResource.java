12324744e5f57372012244ec0f706e9323394d85
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-16 00:23:04 +0530

    Post finally created

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -27,6 +27,9 @@ public class UserJPAResource {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@GetMapping("/jpa/users")
 	public List<User> retrieveAllUsers(){
 		return userRepository.findAll();
@@ -62,5 +65,32 @@ public class UserJPAResource {
 		return ResponseEntity.created(location).build();
 	}
 	
+	@GetMapping("/jpa/users/{id}/posts")
+	public List<Post> retrieveAllUsers(@PathVariable(value = "id") int id){
+		Optional<User> userOptional=userRepository.findById(id);
+		if(!userOptional.isPresent())
+			throw new UserNotFoundException("id-"+id);
+		
+		return userOptional.get().getPosts();
+	}
 	
+	@PostMapping("/jpa/users/{id}/posts")
+	public ResponseEntity<Object> createPost(@PathVariable int id,@RequestBody Post post) {
+		Optional<User> userOptional=userRepository.findById(id);
+		if(!userOptional.isPresent())
+			throw new UserNotFoundException("id-"+id);
+		
+		User user = userOptional.get();
+		post.setUser(user);
+		
+		postRepository.save(post);
+		
+		URI location= ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(post.getId())
+		.toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
 }
